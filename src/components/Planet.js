4f8d863ead9e12471a1a3099b62c6eaeb93e3eab
Domahawk <d.biscan.stu@gmail.com>
2021-04-28 15:06:39 +0200

    planet background and box shadow

--- src/components/Planet.js
+++ src/components/Planet.js
@@ -11,18 +11,21 @@ const Planet = ({ name, population, gravity, terrain, residents }) => {
   })
 
   return (
-    <div>
-    <h1 className='planetName'>{name}</h1>
-      <div className='planetItems'>
-        <h3>Population:</h3>
-        <p>{population}</p>
-        <h3>Gravity:</h3>
-        <p>{gravity}</p>
-        <h3>Terrain:</h3>
-        <p>{terrain}</p>
-        <h3>Characters from here:</h3>
-        <div>{residentListComponent}</div>
-      </div>
+    <div className='planetAll'>
+      <div className='planetBackground'>
+        <h1 className='planetName'>{name}</h1>
+        <div className='planetItems'>
+          <h3>Population:</h3>
+          <p>{population}</p>
+          <h3>Gravity:</h3>
+          <p>{gravity}</p>
+          <h3>Terrain:</h3>
+          <p>{terrain}</p>
+          <h3>Characters from here:</h3>
+          <div>{residentListComponent}</div>
+        </div>  
+    </div>
+
     </div>
   );
 }
